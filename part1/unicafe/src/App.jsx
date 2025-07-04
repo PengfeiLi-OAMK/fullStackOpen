a16c5e09dd4de9c76d8f0d0bbae46f87a122e5fe
PengfeiLi-OAMK <t3lipe01@students.oamk.fi>
2025-07-04 12:42:47 +0300

    completed App anecdotes(1.12-1.14)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,50 +1,55 @@
 import { useState } from "react";
 
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good == 0 && neutral == 0 && bad == 0) {
+    return (
+      <>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={good + neutral + bad} />
+        <StatisticLine
+          text="average"
+          value={(good * 1 - bad * 1) / (good + neutral + bad)}
+        />
+        <StatisticLine
+          text="positive"
+          value={(good / (good + neutral + bad)) * 100 + " %"}
+        />
+      </tbody>
+    </table>
+  );
+};
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const StatisticLine= ({text, value}) => {
-    return (
-      <tr>
-        <td>{text}</td>
-        <td>{value}</td>
-      </tr>
-    );
-  };
-
-  const Statistics = ({good,neutral,bad}) => {
-    if(good==0 && neutral==0 && bad==0) {
-      return (
-        <>
-          <p>No feedback given</p>
-        </>
-      );
-    }
-     return (
-       <table>
-         <tbody>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={good + neutral + bad} />
-          <StatisticLine text="average" value={(good * 1 - bad * 1) / (good + neutral + bad)} />
-          <StatisticLine text="positive" value={(good / (good + neutral + bad)) * 100 + " %"} />
-        </tbody>
-       </table>
-     );
-  };
-
-  const Button= ({onClick, text}) => {
-    return(
-      <button onClick={onClick}>
-        {text}
-      </button>
-    )
-  }
-
+ 
   return (
     <div>
       <h1>give feedback</h1>
